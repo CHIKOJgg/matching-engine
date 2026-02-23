dac272360d392d58f7ce77184f98a9bdd794dd2e
Miraslau <miroslav131206@gmail.com>
2026-02-23 23:32:21 +0300

    add bid/ask

--- src/main/java/org/example/OrderBook.java
+++ src/main/java/org/example/OrderBook.java
@@ -1,10 +1,14 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.TreeMap;
 
 public class OrderBook {
     private  HashMap<String, Order> ordersMap = new HashMap<>();
+    TreeMap<BigDecimal, Deque<Order>> bids;
+    TreeMap<BigDecimal, Deque<Order>> asks;
     void  addOrder(Order order){
         ordersMap.put(order.getId(),order);
     }
